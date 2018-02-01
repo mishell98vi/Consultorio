401db067f29436f455b95d5b988d7e110740c83e
Mishell <>
2018-02-01 09:43:20 -0500

    Consultorio

---
+++ Consultorio/test/consultorio/test/TratamientoTest.java
@@ -0,0 +1,16 @@
+
+package consultorio.test;
+import consultorio.negocio.dao.*;
+import consultorio.negocio.entidades.*;
+import consultorio.negocio.impl.*;
+import java.util.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TratamientoTest {
+    
+    public TratamientoTest() {
+    }
+    
+}
